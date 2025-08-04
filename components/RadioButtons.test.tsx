35131ed7d4b2f6d24c0fa1feef560593ebcad969
jacksonzal <j.g.zaloumis@gmail.com>
2025-08-05 07:42:10 +1000

    ✅ RadioButtons tests

---
+++ components/RadioButtons.test.tsx
@@ -0,0 +1,30 @@
+import { fireEvent, render } from "@testing-library/react-native";
+import { RadioButtons } from "./RadioButtons";
+
+const mockOnChange = jest.fn();
+const mockProps = {
+  label: "The label",
+  selectedValue: "value2",
+  onChange: mockOnChange,
+  options: [
+    { label: "label1", value: "value1" },
+    { label: "label2", value: "value2", image: "theImage" as any },
+  ],
+};
+
+describe("RadioButtons", () => {
+  it("should match snapshot", () => {
+    const container = render(<RadioButtons {...mockProps} />);
+
+    expect(container.toJSON()).toMatchSnapshot();
+  });
+
+  describe("When user presses an option", () => {
+    it("should call onChange with value and show as selected", () => {
+      const { getByText } = render(<RadioButtons {...mockProps} />);
+
+      fireEvent.press(getByText("label1"));
+      expect(mockOnChange).toHaveBeenCalledWith("value1");
+    });
+  });
+});
