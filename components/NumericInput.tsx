7242188d3acccd126a9ed81ceb3b7c71cbdcef10
jacksonzal <j.g.zaloumis@gmail.com>
2025-08-05 07:29:12 +1000

    ✨ Add form control to inputs

--- components/NumericInput.tsx
+++ components/NumericInput.tsx
@@ -1,8 +1,24 @@
-import { TextInput, TextInputProps } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TextInputProps,
+  View,
+} from "react-native";
+import { ErrorMessage } from "./ErrorMessage";
 
-type Props = TextInputProps;
+type Props = TextInputProps & {
+  label: string;
+  errorMessage?: string;
+};
 
-export const NumericInput = ({ onChangeText, ...props }: Props) => {
+export const NumericInput = ({
+  label,
+  errorMessage,
+  onChangeText,
+  style,
+  ...props
+}: Props) => {
   const handleChangeText = (value: string) => {
     // Remove any non-numeric characters except decimal point
     const nextValue = value.replace(/[^0-9.]/g, "");
@@ -11,10 +27,27 @@ export const NumericInput = ({ onChangeText, ...props }: Props) => {
   };
 
   return (
-    <TextInput
-      keyboardType="decimal-pad"
-      onChangeText={handleChangeText}
-      {...props}
-    />
+    <View style={styles.container}>
+      <Text>{label}</Text>
+      <TextInput
+        style={[styles.input, style]}
+        keyboardType="decimal-pad"
+        onChangeText={handleChangeText}
+        {...props}
+      />
+      <ErrorMessage message={errorMessage} />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 4,
+  },
+  input: {
+    fontSize: 16,
+    padding: 6,
+    borderWidth: 1,
+    borderColor: "gray",
+  },
+});
--- components/RadioButtons.tsx
+++ components/RadioButtons.tsx
@@ -1,33 +1,49 @@
 import React from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import { ErrorMessage } from "./ErrorMessage";
 
 type Props = {
+  label: string;
+  errorMessage?: string;
   options: { label: string; value: string }[];
   selectedValue: string;
   onChange: (value: string) => void;
 };
 
-export const RadioButtons = ({ options, selectedValue, onChange }: Props) => {
+export const RadioButtons = ({
+  label,
+  errorMessage,
+  options,
+  selectedValue,
+  onChange,
+}: Props) => {
   return (
     <View style={styles.container}>
-      {options.map(({ label, value }) => {
-        const isSelected = selectedValue === value;
-        return (
-          <Pressable
-            key={value}
-            style={[styles.button, isSelected && styles.buttonSelected]}
-            onPress={() => onChange(value)}
-          >
-            <Text style={isSelected && styles.textSelected}>{label}</Text>
-          </Pressable>
-        );
-      })}
+      <Text>{label}</Text>
+      <View style={styles.buttonsContainer}>
+        {options.map(({ label, value }) => {
+          const isSelected = selectedValue === value;
+          return (
+            <Pressable
+              key={value}
+              style={[styles.button, isSelected && styles.buttonSelected]}
+              onPress={() => onChange(value)}
+            >
+              <Text style={isSelected && styles.textSelected}>{label}</Text>
+            </Pressable>
+          );
+        })}
+      </View>
+      <ErrorMessage message={errorMessage} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    gap: 8,
+  },
+  buttonsContainer: {
     flexDirection: "row",
     borderWidth: 1,
     borderColor: "gray",
