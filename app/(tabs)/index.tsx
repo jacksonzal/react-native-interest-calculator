77843310c2ad99d90e65215910152b64bbe52b78
jacksonzal <j.g.zaloumis@gmail.com>
2025-08-05 07:34:40 +1000

    ✨ InterestForm view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,27 @@
-import { SafeAreaView, Text, } from 'react-native';
+import { InterestForm } from "@/components/InterestForm";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
+    <SafeAreaView style={styles.safeAreaView}>
+      <View style={styles.container}>
+        <Text style={styles.heading}>Interest Calculator</Text>
+        <InterestForm />
+      </View>
     </SafeAreaView>
   );
 }
 
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    gap: 24,
+  },
+  heading: {
+    fontSize: 24,
+  },
+});
---
+++ components/InterestForm.tsx
@@ -0,0 +1,115 @@
+import {
+  INTEREST_FREQUENCY_OPTIONS,
+  InterestFrequency,
+} from "@/constants/interest";
+import { validatePositiveNumber } from "@/utils/validators";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { Button, StyleSheet, View } from "react-native";
+import { NumericInput } from "./NumericInput";
+import { RadioButtons } from "./RadioButtons";
+
+type InterestFormValues = {
+  startAmount: number;
+  interestRate: number;
+  investmentTerm: number;
+  frequency: InterestFrequency;
+};
+
+export const InterestForm = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InterestFormValues>({
+    defaultValues: {
+      frequency: "monthly",
+    },
+  });
+
+  const onSubmit: SubmitHandler<InterestFormValues> = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Controller
+        control={control}
+        name="startAmount"
+        rules={{
+          required: "Start deposit amount is required",
+          validate: validatePositiveNumber,
+        }}
+        render={({ field: { onChange, value, ...field } }) => (
+          <NumericInput
+            label="Start deposit amount"
+            onChangeText={onChange}
+            value={value?.toString() || ""}
+            errorMessage={errors.startAmount?.message}
+            {...field}
+          />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name="interestRate"
+        rules={{
+          required: "Interest rate is required",
+          validate: validatePositiveNumber,
+        }}
+        render={({ field: { onChange, value, ...field } }) => (
+          <NumericInput
+            label="Interest rate"
+            onChangeText={onChange}
+            value={value?.toString() || ""}
+            errorMessage={errors.interestRate?.message}
+            {...field}
+          />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name="investmentTerm"
+        rules={{
+          required: "Investment term is required",
+          validate: validatePositiveNumber,
+        }}
+        render={({ field: { onChange, value, ...field } }) => (
+          <NumericInput
+            label="Investment term (years)"
+            onChangeText={onChange}
+            value={value?.toString() || ""}
+            errorMessage={errors.investmentTerm?.message}
+            {...field}
+          />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name="frequency"
+        rules={{
+          required: "You must select an option",
+        }}
+        render={({ field: { onChange, value } }) => (
+          <RadioButtons
+            label="Interest paid frequency"
+            selectedValue={value}
+            onChange={onChange}
+            options={INTEREST_FREQUENCY_OPTIONS}
+          />
+        )}
+      />
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 16,
+  },
+});
